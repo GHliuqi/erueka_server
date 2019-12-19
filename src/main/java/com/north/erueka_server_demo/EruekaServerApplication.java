d82074b7de6ddd148e0f8f51509a0e32e2eac60d
liuqi <liuqi@ebnew.com>
2019-12-19 15:19:01 +0800

    Initial commit

---
+++ src/main/java/com/north/erueka_server_demo/EruekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.north.erueka_server_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EruekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EruekaServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/north/erueka_server_demo/EruekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.north.erueka_server_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EruekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
